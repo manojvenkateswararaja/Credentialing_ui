92810a29fd4b8b033c34fd9b92e8749b416c0a04
kheteshR <khetaram.rotangan@rapidqube.com>
2018-01-12 10:37:55 +0530

    all hbs file changes

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,22 +2,23 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(){
-        // this.transitionTo('userloanschedule')14
+        //this.transitionTo('userloanschedule')14
         //this.transitionTo('propertydetail')
-        // this.transitionTo('bankdashboard')9
-        // this.transitionTo('userdetails')8
-        // this.transitionTo('userdetailsdec')13
-        // this.transitionTo('loanschedule')6
+        //this.transitionTo('bankdashboard')9
+        //this.transitionTo('userdetails')8
+        //this.transitionTo('userdetailsdec')13
+        //this.transitionTo('loanschedule')6
         //this.transitionTo('loanquotation')
-        // this.transitionTo('newrequest')7
-        // this.transitionTo('bankdashboard')3
-        // this.transitionTo('legalverification')10
+        //this.transitionTo('newrequest')7
+        //this.transitionTo('bankdashboard')3
+        //this.transitionTo('legalverification')10
         //this.transitionTo('legalverification2')
-        // this.transitionTo('creditscore')11
-        // this.transitionTo('FillCreditScore')12
-        this.transitionTo('uploaddoc')
-        // this.transitionTo('creditscore')
-        // this.transitionTo('login')2
-        // this.transitionTo('signup')1
+        //this.transitionTo('creditscore')11
+        //this.transitionTo('FillCreditScore')12
+        //this.transitionTo('uploaddoc')
+        //this.transitionTo('creditscore')
+        this.transitionTo('login')
+        //this.transitionTo('signup')1
+        // this.transitionTo('home')
     }
 });
