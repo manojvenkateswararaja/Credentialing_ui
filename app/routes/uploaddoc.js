9bd8e3d1e1efcdd9dc17958eabde8488a2832a88
kheteshR <khetaram.rotangan@rapidqube.com>
2018-02-12 10:41:43 +0530

    upload docs work

--- app/routes/uploaddoc.js
+++ app/routes/uploaddoc.js
@@ -4,17 +4,12 @@ export default Route.extend({
     actions:{
         uploadDoc:function (file) {
             // var mycontroller = this;
-    console.log("entering upload FIR 3");
-    var mycontroller = this;
-    
-
-    console.log(file)
+   console.log("entering upload FIR 3");
+   var mycontroller = this;
+   console.log(file)
    var requestid=this.controllerFor('newrequest').get('reqid');
    this.controllerFor('uploaddoc').set('requestid',requestid);
    console.log(requestid);
-
-
-
 file.upload('http://localhost:8082/UploadDocs?requestid='+requestid).then(function (response) {
     console.log(JSON.stringify(response));
     var url =response.body.url;
@@ -26,20 +21,16 @@ file.upload('http://localhost:8082/UploadDocs?requestid='+requestid).then(functi
     mycontroller.controllerFor('uploaddoc').set("isShow_fileupload1",true);
     mycontroller.controllerFor('uploaddoc').set("Notshow_fileupload1",false);
     console.log("saviing file...");
-    console.log("file upload sucessfully. 1..");
-   
-    
+    console.log("file upload sucessfully. 1..");  
  }, function () {
-   
     console.log("file upload sucessfully...");
- 
   });
 },
-    },
+    },   
 model(){
     this.controllerFor('uploaddoc').set('Notshow_fileupload', true); 
     if(this.controllerFor('uploaddoc').set('Notshow_fileupload', true)){
-     this.controllerFor('uploaddoc').set(' isShow_fileupload', false); 
+    this.controllerFor('uploaddoc').set(' isShow_fileupload', false); 
     }  
     this.controllerFor('uploaddoc').set('Notshow_fileupload1', true); 
     if(this.controllerFor('uploaddoc').set('Notshow_fileupload1', true)){
@@ -51,4 +42,4 @@ model(){
 }
    
 });
-// var requestid=mycontroller.controllerFor('page4').get('reqid');
+
